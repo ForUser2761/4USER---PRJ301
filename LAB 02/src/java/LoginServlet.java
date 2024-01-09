85ff068c4938c1326c2c961020561d8783c6a56e
foruser <vinhpham2761.gmail.com>
2024-01-10 03:42:14 +0700

    LAB 02

---
+++ LAB 02/src/java/LoginServlet.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("pass");
+        //•	It will check {user = abc and password = 12345}
+        boolean isCorrect = false;
+        if (name.equals("abc") && password.equals("12345")) {
+            isCorrect = true;
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            if (isCorrect) {
+                out.println("<h1> LoginSuccess" + "</h1>");
+            } else {
+                out.println("<h1> Login Failed" + "</h1>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
